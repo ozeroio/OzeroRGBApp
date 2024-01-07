3fdfe2b603a803e5abb4a5f92076f6f18a74d8b2
Dalmir da Silva <dalmirdasilva@gmail.com>
2024-01-06 23:54:24 -0700

    build: update deployment files

--- ops/aws_deploy.sh
+++ ops/aws_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-npm run build
+npm run build-aws
 aws s3 rm s3://rgb.ozero.io --recursive --profile aws0:devops
 aws s3 cp  --recursive dist/ozero-rgb-app/ s3://rgb.ozero.io --profile aws0:devops
